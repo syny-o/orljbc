0d83ddb8c5a6c2270f947f38be60b709434aa809
ondrejsynek <ondrej.synek@virtuell.cz>
2025-12-11 19:58:49 +0100

    adjusted for vercel deployment

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,23 @@
-import { PrismaClient } from "@/generated/prisma/client";
-
-import { PrismaPg } from '@prisma/adapter-pg'
-import 'dotenv/config'
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-})
-
-const prisma = new PrismaClient({
-  adapter,
+  connectionString: process.env.DATABASE_URL!,
 });
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+    log: ["error", "warn"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
